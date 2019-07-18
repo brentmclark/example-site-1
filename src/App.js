a933324533738b443c4da4849334e2ceae0ebf5f
Alex Lenzen <alenzen@live.com>
2019-07-18 14:58:38 -0500

    Update 2

--- src/App.js
+++ src/App.js
@@ -1,29 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="container">
+          <h1>Coming soon to a galaxy near you</h1>
+          <p className="border"></p>
+          <p className="days">10,000 days left</p>
+        </div>
         <div className="App-Logo2">
           <p>Logo</p>
         </div>
         <div className="lowerLeft">
-          <p>Powered by <a href="https://en.wikipedia.org/wiki/Coffee">
-          AL and BC</a>
+          <p className="pb">Powered by <a href="https://en.wikipedia.org/wiki/Coffee" 
+          target="_blank" rel="noopener noreferrer">AL and BC</a>
           </p>
         </div>
       </header>
